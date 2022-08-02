03949fc1c9625ae0e2205ec83d75676d1f6dbfad
Marvish <mschandr@uci.edu>
2022-08-02 12:01:20 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Marvish Chandra
+
+''' Find the properties of an element given these found characteristics'''
+
+def __init__(self,mass,temperature):
+    self.mass = mass
+    self.temperature = temperature
+
+
+
+def findDensity(self,volume):
+    pConstant = mass / v
+    print("The given density of an element is:" + str(pConstant) + ".")
+
+
+def find_Melting_Point(temperature):
+    meltingP = temperature * mass
+    print("The given melting point of an element is:" + str(meltingP) + ".")
+
+
+def find_Boiling_Point(temperature):
+    boilingP = temperature * mass
+    print("The given boiling point of an element is: +" str(boilingP) + ".")
+
+
+def find_Electric_Conductivity(radius,length,mV):
